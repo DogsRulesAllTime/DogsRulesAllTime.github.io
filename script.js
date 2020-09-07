70394daa2f24560a75cc7d08e827c97eeb8db93f
DogsRulesAllTime <kuznetsoffar@ya.ru>
2020-09-07 18:09:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert('11');
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Привет, javascript!";
+    }
+
+function openk() {
+    // alert('open')
+    document.getElementById('nav_menu_slide').style.cssText = 'right: 0vw;';
+}
+
+function closek() {
+    // alert('close')
+    document.getElementById('nav_menu_slide').style.cssText = 'right: -50vw;';
+}
